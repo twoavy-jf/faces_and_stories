a2b122fb03b768616f7e7b235b5da66af29c6330
Anna <a.parlapani@gmx.net>
2019-10-21 09:12:31 +0200

    init cat grid structure cat slider

---
+++ src/components/cat-grid.vue
@@ -0,0 +1,53 @@
+<template>
+    <div ref="grid">
+        <img src="http://placekitten.com/g/1000/600" class="cat" @load="loaded">
+        <img src="http://placekitten.com/g/900/700" class="cat" @load="loaded">
+        <img src="http://placekitten.com/g/600/200" class="cat" @load="loaded">
+        <img src="http://placekitten.com/g/800/500" class="cat" @load="loaded">
+        <img src="http://placekitten.com/g/950/800" class="cat big-cat" @load="loaded">
+        <img src="http://placekitten.com/g/1200/700" class="cat big-cat" @load="loaded">
+        <img src="http://placekitten.com/g/300/700" class="cat" @load="loaded">
+        <img src="http://placekitten.com/g/1000/200" class="cat" @load="loaded">
+        <img src="http://placekitten.com/g/500/200" class="cat" @load="loaded">
+        <img src="http://placekitten.com/g/700/700" class="cat" @load="loaded">
+    </div>
+</template>
+
+<script>
+import Masonry from 'masonry-layout'
+
+export default {
+    name: 'cat-grid',
+    data() {
+        return {
+            msnry: null,
+            numLoaded: 0
+        }
+    },
+    methods: {
+        loaded() {
+            this.numLoaded++
+            if (this.numLoaded >= 10) {
+                this.init()
+            }
+        },
+        init() {
+            this.msnry = new Masonry(this.$refs.grid, {
+                // options
+                itemSelector: '.cat',
+                columnWidth: 300
+            })
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .cat {
+        width: 300px;
+        padding: 5px;
+    }
+    .big-cat {
+        width: 600px;
+    }
+</style>
---
+++ src/components/cat-slider.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <slick ref="slick" :options="options">
+            <img src="http://placekitten.com/g/1000/600" class="cat">
+            <img src="http://placekitten.com/g/900/700" class="cat">
+            <img src="http://placekitten.com/g/800/500" class="cat">
+            <img src="http://placekitten.com/g/950/800" class="cat">
+            <img src="http://placekitten.com/g/1200/700" class="cat">
+        </slick>
+        <div class="position-absolute d-flex justify-content-between controls">
+            <div class="btn btn-primary" @click="prev">prev</div>
+            <div class="btn btn-primary" @click="next">next</div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Slick from 'vue-slick'
+
+export default {
+    name: 'cat-slider',
+    components: { Slick },
+    data() {
+        return {
+            options: {
+                arrows: false
+            }
+        }
+    },
+    // mounted() {
+    //     this.$nextTick(() => {
+    //         this.$refs.slick.reSlick()
+    //     })
+    // }
+    methods: {
+        next() {
+            this.$refs.slick.next()
+        },
+
+        prev() {
+            this.$refs.slick.prev()
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    @import "~slick-carousel/slick/slick.css";
+
+    .cat {
+        max-height: 100vh;
+    }
+
+    .controls {
+        top: 50%;
+        width: 100%;
+    }
+
+</style>
