10efcd10aa67fe0fdff1f100be6fb7c4499637db
Anna <a.parlapani@gmx.net>
2019-10-23 16:08:32 +0200

    popup mobile phone version

--- src/components/cat-popup.vue
+++ src/components/cat-popup.vue
@@ -23,6 +23,33 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    @media only screen and (max-width: 600px) {
+        .popup {
+            width: 100vw;
+            height: 100vh;
+            top: 0;
+            left: 0;
+            position: fixed;
+            z-index: 10;
+            background: rgba(0, 0, 0, 0.8);
+
+            .image-container {
+                width: 90%;
+                height: 90%;
+
+                img {
+                    max-width: 100%;
+                    max-height: 100%;
+                }
+            }
+            .close-button {
+                position: absolute;
+                top: 0;
+                right: 0;
+                margin: 5px;
+            }
+        }
+    }
     .popup {
         width: 100vw;
         height: 100vh;
